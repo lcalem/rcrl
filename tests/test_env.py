3b68fe4c4a35fe414a6730187401a690480db6f4
lcalem <laura.calem@gmail.com>
2018-08-10 16:15:38 +0200

    fixed permutation matrices

--- tests/test_env.py
+++ tests/test_env.py
@@ -14,7 +14,41 @@ SOLVED_CUBE = np.matrix([
 ])
 
 
-def test_rotations():
+def test_l():
+    expected_state = np.matrix([1,0,0,1,0,0,1,0,0, 2,1,1,2,1,1,2,1,1, 2,2,3,2,2,3,2,2,3, 0,3,3,0,3,3,0,3,3, 4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5])
+    starting_cube = SOLVED_CUBE
+    env = CubeEnvironment()
+    env.init_cube(cube_repr=starting_cube)
+    new_state, reward, solved = env.take_action('u')
+    new_state, reward, solved = env.take_action('u')
+    print("NEW STATE %s" % str(new_state))
+    new_state, reward, solved = env.take_action('R')
+    print("NEW STATE %s" % str(new_state))
+    new_state, reward, solved = env.take_action('u')
+    new_state, reward, solved = env.take_action('u')
+    print("NEW STATE %s" % str(new_state))
+    assert np.array_equal(new_state, expected_state)
+
+
+
+@pytest.mark.parametrize(["action", "expected_state"], [
+
+    ["r",  np.matrix([3,3,3,3,3,3,3,3,3, 0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1, 2,2,2,2,2,2,2,2,2, 4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5])],
+    ["u",  np.matrix([4,4,4,4,4,4,4,4,4, 1,1,1,1,1,1,1,1,1, 5,5,5,5,5,5,5,5,5, 3,3,3,3,3,3,3,3,3, 2,2,2,2,2,2,2,2,2, 0,0,0,0,0,0,0,0,0])],
+    ["R",  np.matrix([0,0,3,0,0,3,0,0,3, 1,1,0,1,1,0,1,1,0, 1,2,2,1,2,2,1,2,2, 3,3,2,3,3,2,3,3,2, 4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5])],
+    ["R'", np.matrix([0,0,1,0,0,1,0,0,1, 1,1,2,1,1,2,1,1,2, 3,2,2,3,2,2,3,2,2, 3,3,0,3,3,0,3,3,0, 4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5])],
+    ["L",  np.matrix([1,0,0,1,0,0,1,0,0, 2,1,1,2,1,1,2,1,1, 2,2,3,2,2,3,2,2,3, 0,3,3,0,3,3,0,3,3, 4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5])],
+    ["L'", np.matrix([3,0,0,3,0,0,3,0,0, 0,1,1,0,1,1,0,1,1, 2,2,1,2,2,1,2,2,1, 2,3,3,2,3,3,2,3,3, 4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5])],
+    ["F",  np.matrix([0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,5,5,5, 2,2,2,2,2,2,2,2,2, 4,4,4,3,3,3,3,3,3, 1,4,4,1,4,4,1,4,4, 5,5,3,5,5,3,5,5,3])],
+    ["F'", np.matrix([0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,4,4,4, 2,2,2,2,2,2,2,2,2, 5,5,5,3,3,3,3,3,3, 3,4,4,3,4,4,3,4,4, 5,5,2,5,5,2,5,5,2])],
+    # U
+    # U'
+    # D
+    # D'
+    # B
+    # B'
+])
+def test_rotations(action, expected_state):
     '''
     starts with a known cube representation, take an action and checks that the 
     resulting state matches what we expect after this state
@@ -27,22 +61,15 @@ def test_rotations():
     4: 'Red',
     5: 'Orange'
     '''
-    expected_state = np.matrix([
-        0, 0, 3, 0, 0, 3, 0, 0, 3,
-        1, 1, 0, 1, 1, 0, 1, 1, 0,
-        2, 2, 1, 2, 2, 1, 2, 2, 1, 
-        3, 3, 2, 3, 3, 2, 3, 3, 2,        
-        4, 4, 4, 4, 4, 4, 4, 4, 4,
-        5, 5, 5, 5, 5, 5, 5, 5, 5
-    ])
 
     starting_cube = SOLVED_CUBE
     env = CubeEnvironment()
     env.init_cube(cube_repr=starting_cube)
-    new_state, reward, solved = env.take_action('R')
+    new_state, reward, solved = env.take_action(action)
 
     assert reward == 0
     assert solved is False
+    print("NEW STATE %s" % str(new_state))
     assert np.array_equal(new_state, expected_state)
 
 
@@ -62,6 +89,3 @@ def test_solved():
     assert solved is True
     assert np.array_equal(new_state, starting_cube)
 
-
-if __name__ == '__main__':
-    test_rotations()
